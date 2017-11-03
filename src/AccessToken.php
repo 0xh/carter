158ed407dc9b419f3b4239c796ffe9719e3a277e
Nicky Woolf <woolfnicky@gmail.com>
2017-11-02 17:25:46 -0700

    Test Access Token

--- src/AccessToken.php
+++ src/AccessToken.php
@@ -2,7 +2,23 @@
 
 namespace NickyWoolf\Thrust;
 
+use NickyWoolf\Shopify\Shopify;
+
 class AccessToken
 {
-    //
+    protected $shopify;
+
+    public function __construct(Shopify $shopify)
+    {
+        $this->shopify = $shopify;
+    }
+
+    public function request($code)
+    {
+        return $this->shopify->post('oauth/access_token', [
+            'client_id' => config('thrust.client_id'),
+            'client_secret' => config('thrust.client_secret'),
+            'code' => $code,
+        ])->json();
+    }
 }
---
+++ tests/Unit/AccessTokenTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use Mockery;
+use NickyWoolf\Shopify\Shopify;
+use NickyWoolf\Thrust\AccessToken;
+use Tests\TestCase;
+
+class AccessTokenTest extends TestCase
+{
+    /** @test */
+    function makes_request_to_shopify()
+    {
+        config()->set('thrust', [
+            'client_id' => 'CLIENT-ID',
+            'client_secret' => 'CLIENT-SECRET',
+        ]);
+        $shopify = Mockery::mock(Shopify::class);
+        $accessToken = new AccessToken($shopify);
+        $shopify->shouldReceive('post')->with('oauth/access_token', [
+            'client_id' => 'CLIENT-ID',
+            'client_secret' => 'CLIENT-SECRET',
+            'code' => 'CODE-FROM-SHOPIFY',
+        ])->once()->andReturnSelf();
+        $shopify->shouldReceive('json')->once()->andReturn([
+            'access_token' => 'ACCESS-TOKEN',
+            'scope' => 'READ,WRITE',
+        ]);
+
+        $response = $accessToken->request('CODE-FROM-SHOPIFY');
+
+        $this->assertEquals([
+            'access_token' => 'ACCESS-TOKEN',
+            'scope' => 'READ,WRITE',
+        ], $response);
+    }
+}
