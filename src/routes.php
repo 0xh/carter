2020b644a65fc91e184176542c675c77bc4bf43f
Nicky Woolf <woolfnicky@gmail.com>
2017-11-03 06:53:10 -0700

    formatting

--- src/routes.php
+++ src/routes.php
@@ -4,21 +4,23 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use NickyWoolf\Thrust\Http\Middleware\HasShopDomain;
 use NickyWoolf\Thrust\Http\Middleware\HasValidHmac;
-use NickyWoolf\Thrust\Http\Middleware\HasValidNonce;
 use NickyWoolf\Thrust\Http\Middleware\HasValidHostname;
+use NickyWoolf\Thrust\Http\Middleware\HasValidNonce;
 
 Route::group(['namespace' => 'NickyWoolf\\Thrust\\Http\\Controllers'], function (Router $router) {
 
     $router->get('shopify/signup', 'InstalledAppsController@create')
         ->name('thrust.signup');
 
-    $router->match(['get', 'post'], 'shopify/install', 'InstalledAppsController@store')
-        ->middleware([HasShopDomain::class])
-        ->name('thrust.install');
+    $router->match(['get', 'post'], 'shopify/install', 'InstalledAppsController@store')->middleware([
+        HasShopDomain::class,
+    ])->name('thrust.install');
 
-    $router->get('shopify/register', 'RegisteredShopsController@store')
-        ->middleware([HasValidNonce::class, HasValidHmac::class, HasValidHostname::class])
-        ->name('thrust.register');
+    $router->get('shopify/register', 'RegisteredShopsController@store')->middleware([
+        HasValidNonce::class,
+        HasValidHmac::class,
+        HasValidHostname::class,
+    ])->name('thrust.register');
 
     $router->get('shopify/dashboard', 'DashboardController@index')
         ->name('thrust.dashboard');
