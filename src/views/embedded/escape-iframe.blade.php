061af2b144766e1ef426cb0dbc4d10dd373d42f3
Nicky Woolf <woolfnicky@gmail.com>
2017-11-04 06:26:37 -0700

    View To Escape IFrame

---
+++ src/views/embedded/escape-iframe.blade.php
@@ -0,0 +1,10 @@
+@extends('launch::embedded')
+
+@section('script')
+    <script>
+        window.parent.postMessage(JSON.stringify({
+            message: 'Shopify.API.remoteRedirect',
+            data: { location: '{{ $redirect }}' }
+        }), 'https://{{ auth()->user()->shopify_domain }}');
+    </script>
+@stop
