1e879fbbb907acc0778abff614faeccf1d209ca0
Nicky Woolf <woolfnicky@gmail.com>
2017-11-04 05:57:34 -0700

    Add Default Dashboard View

--- src/views/embedded.blade.php
+++ src/views/embedded.blade.php
@@ -1,10 +1,18 @@
-@extends('master')
+@extends('launch::master')
 
 @section('head')
+    <script src="https://cdn.shopify.com/s/assets/external/app.js"></script>
     <script>
-//        Shopify Init
+        ShopifyApp.init({
+            apiKey: "{{ config('launch.client_id') }}",
+            shopOrigin: "{{ ($user = auth()->user()) ? "https://{$user->shopify_domain}" : '' }}",
+            debug: {{ app()->environment('production') ? 'false' : 'true' }},
+        });
     </script>
 @stop
 
 @section('body')
+    @yield('content')
+
+    @yield('script')
 @stop
